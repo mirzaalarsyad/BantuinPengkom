d454f274c4a261b09fd4d9c9f3968c9d4b09cb94
Muhammad Mirza Fathan Al Arsyad <47689911+mirzaalarsyad@users.noreply.github.com>
2019-09-19 20:29:39 +0700

    Create operator.m

---
+++ operator.m
@@ -0,0 +1,16 @@
+x = input('Masukkan angka pertama: ');
+y = input('Masukkan angka kedua: ');
+op = input('Masukkan operator: ', 's');
+
+if op == '+'
+    fprintf('%d + %d = %d\n', x , y, x+y);
+elseif op == '-'
+    fprintf('%d - %d = %d\n', x, y, x-y);
+elseif op == '/'
+    fprintf('%d / %d = %d\n', x, y, floor(x/y));
+elseif op == '%'
+    fprintf('%d', x);
+    fprintf(' %% ');
+    fprintf('%d = ', y);
+    fprintf('%d\n', x - y*floor(x/y));
+end
